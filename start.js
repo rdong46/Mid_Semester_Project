662f2e3a8e4a60ba637c2a4e3a9aee42f5f48837
rdong46 <rickydongcollege@gmail.com>
2022-10-05 09:57:24 -0400

    Update to .js file

--- start.js
+++ start.js
@@ -15,57 +15,9 @@ const leftButton = document.getElementsByClassName("button")[0];
 const rightButton = document.getElementsByClassName("button")[1];
 const pokemon_image = document.getElementById("pokemon_image");
 const type_list = document.getElementById("type-list");
-const info_box = document.getElementById("info-box");
-fetch("https://pokeapi.co/api/v2/pokemon/" + id)
-  .then((resp) => resp.json())
-  .then((resp) => {
-    pokemon_name.textContent = resp.name;
-    pokemon_image.src =
-      resp.sprites["other"]["official-artwork"]["front_default"];
-    for (key in resp.types) {
-      pokemon_type.push(resp.types[key]["type"]["name"]);
-    }
-    for (let i = 0; i < pokemon_type.length; i++) {
-      type_list.innerHTML +=
-        '<div class="type-box ' +
-        pokemon_type[i] +
-        '" class>' +
-        pokemon_type[i] +
-        "</div>";
-    }
-    height = resp.height;
-    weight = resp.weight;
-    hp = resp.stats[0]["base_stat"];
-    attack = resp.stats[1]["base_stat"];
-    defence = resp.stats[2]["base_stat"];
-    special_attack = resp.stats[3]["base_stat"];
-    special_defence = resp.stats[4]["base_stat"];
-    speed = resp.stats[5]["base_stat"];
-    info_box.innerHTML =
-      "<p id='info_text'> height:" +
-      height +
-      "m\nweight: " +
-      weight +
-      "kg\nhp: " +
-      hp +
-      "\nattack: " +
-      attack +
-      "\ndefence " +
-      defense +
-      "\nspecial-attack: " +
-      special_attack +
-      "\nspecial-defence; " +
-      special_defence +
-      "\nspeed: " +
-      speed +
-      "</p>";
-  });
+const info_box = document.getElementById("info_box");
 
-rightButton.addEventListener("click", () => {
-  id = (id + 1) % 899;
-  if (id == 0) {
-    id = 1;
-  }
+function fetchPokemon() {
   fetch("https://pokeapi.co/api/v2/pokemon/" + id)
     .then((resp) => resp.json())
     .then((resp) => {
@@ -85,7 +37,42 @@ rightButton.addEventListener("click", () => {
           pokemon_type[i] +
           "</div>";
       }
+      height = resp.height;
+      weight = resp.weight;
+      hp = resp.stats[0]["base_stat"];
+      attack = resp.stats[1]["base_stat"];
+      defense = resp.stats[2]["base_stat"];
+      special_attack = resp.stats[3]["base_stat"];
+      special_defence = resp.stats[4]["base_stat"];
+      speed = resp.stats[5]["base_stat"];
+      info_box.innerHTML =
+        "<div class='info_text'> height: " +
+        height +
+        "m</div><div class='info_text'> weight: " +
+        weight +
+        "kg</div><div class='info_text'>hp: " +
+        hp +
+        "</div><div class='info_text'>attack: " +
+        attack +
+        "</div><div class='info_text'>defense: " +
+        defense +
+        "</div><div class='info_text'>special-attack: " +
+        special_attack +
+        "</div><div class='info_text'>special-defence; " +
+        special_defence +
+        "</div><div class='info_text'>speed: " +
+        speed +
+        "</div>";
     });
+}
+fetchPokemon();
+
+rightButton.addEventListener("click", () => {
+  id = (id + 1) % 899;
+  if (id == 0) {
+    id = 1;
+  }
+  fetchPokemon();
 });
 
 leftButton.addEventListener("click", () => {
@@ -93,24 +80,5 @@ leftButton.addEventListener("click", () => {
   if (id == 0) {
     id = 898;
   }
-  fetch("https://pokeapi.co/api/v2/pokemon/" + id)
-    .then((resp) => resp.json())
-    .then((resp) => {
-      pokemon_name.textContent = resp.name;
-      pokemon_image.src =
-        resp.sprites["other"]["official-artwork"]["front_default"];
-      pokemon_type = [];
-      type_list.innerHTML = "";
-      for (key in resp.types) {
-        pokemon_type.push(resp.types[key]["type"]["name"]);
-      }
-      for (let i = 0; i < pokemon_type.length; i++) {
-        type_list.innerHTML +=
-          '<div class="type-box ' +
-          pokemon_type[i] +
-          '" class>' +
-          pokemon_type[i] +
-          "</div>";
-      }
-    });
+  fetchPokemon();
 });
